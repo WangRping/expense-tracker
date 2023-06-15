3d567e0788348a341473d1900b17dc60cd8473b6
WangRping <r17369@gmail.com>
2023-06-15 19:39:26 +0800

    add user login page

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const router = require('./routes/index')
 const app = express()
 require('./config/mongoose')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(router)
 
 app.get('/', (req, res) => {
   res.render('index')
--- models/seeds/seeder.js
+++ models/seeds/seeder.js
@@ -7,36 +7,6 @@ const Category = require('../category');
 const category = require('../category');
 const db = require('../../config/mongoose')
 
-// db.once('open', () => {
-//   let categoryId = ''
-//   let userId = ''
-
-//   Category.create({ category_name: '家居物業' }),
-//     Category.create({ category_name: '交通出行' }),
-//     Category.create({ category_name: '休閒娛樂' }),
-//     Category.create({ category_name: '餐飲食品' }),
-//     Category.create({ category_name: '其他' })
-
-//   User.create({ name: 'user', password: 'user', email: 'user@example.com' })
-
-
-//   Category.findOne({ category_name: '家居物業' })
-//     .lean()
-//     .then(category => categoryId = category._id)
-
-//   Category.findOne({ category_name: 'user' })
-//     .lean()
-//     .then(user => userId = user._id)
-
-//   for (let i = 1; i <= 3; i++) {
-//     const amount = Math.floor(Math.random() * 900 + 1)
-//     Record.create({ name: `name-{i}`, userId, categoryId, date: '2023-06-01', amount })
-//   }
-
-//   console.log('done')
-//   process.exit()
-// });
-
 db.once('open', async () => {
   try {
     await Category.create({ category_name: '家居物業' });
