e920d46e44b295a299ca66ac1e463e9d4a705225
WangRping <r17369@gmail.com>
2023-06-16 20:38:02 +0800

    add edit page and router

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const moment = require('moment')
 const router = express.Router()
 const Category = require('../../models/category')
 const Record = require('../../models/record')
+const record = require('../../models/record')
+const category = require('../../models/category')
 
 router.get('/new', (req, res) => {
   Category.find()
@@ -22,6 +25,29 @@ router.post('/:id/delete', (req, res) => {
   Record.findOne({ _id, userId })
     .then(record => record.remove())
     .then(() => res.redirect('/'))
+})
+
+router.get('/:id/edit', async (req, res) => {
+  try {
+    const userId = req.user._id
+    const _id = req.params.id
+    const categorys = await Category.find().lean()
+    const record = await Record.findOne({ userId, _id }).lean()
+
+    for (let i = 0; i < categorys.length; i++) {
+      if (categorys[i]._id.toString() === record.categoryId.toString()) {
+        categorys[i].selected = true
+      }
+    }
+
+    console.log(categorys)
+
+    record.date = moment(record.date).format('YYYY-MM-DD')
+    res.render('edit', { record, categorys })
+
+  } catch (err) {
+    console.log(err)
+  }
 })
 
 module.exports = router
