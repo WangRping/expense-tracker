a37de6913aadbe8dfa06356dfb31f663e8c57294
WangRping <r17369@gmail.com>
2023-06-16 13:37:57 +0800

    modify home page : render by record

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,20 +1,33 @@
 const express = require('express')
+const moment = require('moment');
 const router = express.Router()
 const user = require('./users')
 const Record = require('../../models/record')
+const Category = require('../../models/category')
+const category = require('../../models/category')
 
 // const { authenticator } = require('../middleware/auth')
 
 
-router.get('/', (req, res) => {
 
-  console.log(req.user)
-  const userId = req.user._id
-  Record.find({ userId })
-    .lean()
-    .then(record => console.log(record))
-    .then()
-  res.render('index')
-})
+router.get('/', async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const records = await Record.find({ userId }).lean();
+    const renderRecords = [];
+
+    for (const record of records) {
+      const category = await Category.findOne({ _id: record.categoryId }).lean();
+      record.img = category.img;
+      record.date = moment(record.date).format('YYYY/MM/DD'); // 格式化日期
+      renderRecords.push(record);
+    }
+
+    res.render('index', { records: renderRecords });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 module.exports = router
