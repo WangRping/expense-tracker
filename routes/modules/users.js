58a1b5ec659058571a25f4247a3a5c5fc2116a12
WangRping <r17369@gmail.com>
2023-06-16 21:17:03 +0800

    add register page and router

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -33,4 +33,35 @@ router.get('/logout', (req, res) => {
   })
 })
 
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  const errors = []
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: '所有欄位皆為必填' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符' })
+  }
+  if (errors.length) {
+    console.log(errors)
+    return res.render('register', { name, email, password, confirmPassword })
+  }
+
+  User.findOne({ email })
+    .then(user => {
+      if (user) {
+        console.log('信箱已經註冊')
+        return res.render('register', { name, email, password, confirmPassword })
+      }
+      return User.create({ name, email, password })
+        .then(() => res.redirect('/'))
+        .catch(err => console.log(err))
+    })
+
+})
+
 module.exports = router
