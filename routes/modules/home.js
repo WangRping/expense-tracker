5f5a090352d4a1a58898d109d3bca23842807dbb
WangRping <r17369@gmail.com>
2023-06-16 14:58:00 +0800

    add category router to render index

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -30,4 +30,27 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/category/:categoryId', async (req, res) => {
+  try {
+    const categoryId = req.params.categoryId
+    const userId = req.user._id
+    const renderRecords = []
+    const records = await Record.find({ categoryId, userId }).lean()
+    const categoryImg = await Category.findOne({ _id: categoryId }).lean().then(category => { return category.img })
+
+    for (const record of records) {
+      record.img = categoryImg;
+      record.date = moment(record.date).format('YYYY/MM/DD'); // 格式化日期
+      renderRecords.push(record);
+    }
+
+    const categorys = await Category.find().lean()
+    res.render('index', { records: renderRecords, categorys });
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
+})
+
 module.exports = router
