3999aacf55e134483d0694a66e901b7be49c4719
WangRping <r17369@gmail.com>
2023-06-16 23:20:13 +0800

    record UI/US design

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,8 +1,20 @@
+// 計算總金額
 const amount = document.querySelectorAll('.record-amout')
-const amoutWrapper = document.querySelector('.amout-wrapper')
+const amountWrapper = document.querySelector('.amount-wrapper')
 
 let total = 0
 
 amount.forEach(item => total += parseInt(item.textContent))
+amountWrapper.innerHTML += `<h1>${total}</h1>`
 
-amoutWrapper.innerHTML += `<h1>${total}</h1>`
+// card顏色改變
+const cards = document.querySelectorAll('.card')
+const cardQTY = cards.length
+
+for (let i = 0; i <= cards.length; i += 2) {
+  cards[i].style.backgroundColor = '#f4f3f4';
+}
+
+for (let i = 1; i <= cards.length; i += 2) {
+  cards[i].style.backgroundColor = '#ffffff';
+}
